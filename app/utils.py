5c5c739424cb30890f27793280de89e6f709da63
abdoula14cherif <abdoula14cherif@gmail.com>
2026-09-22 20:40:10 +0100

    Delete utils.py

--- app/utils.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import string
-
-
-def generate_referral_code(length: int = 8) -> str:
-    alphabet = string.ascii_uppercase + string.digits
-    return "".join(secrets.choice(alphabet) for _ in range(length))
